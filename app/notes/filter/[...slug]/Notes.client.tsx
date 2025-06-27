b4105438b87ff3b62d659c8d86e66dfed53afddc
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-06-27 15:33:09 +0300

    delete

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { Note } from "@/types/note";
-import NotesClient from "@/app/notes/Notes.client";
+import NotesClient from "./Notes.client";
 
 interface Props {
   notes: Note[];
--- app/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-import { fetchNotes } from "../../lib/api";
-import NotesClient from "./Notes.client";
-
-export default async function NotesPage() {
-  const data = await fetchNotes();
-
-  return <NotesClient notes={data.notes} totalPages={data.totalPages} />;
-}
